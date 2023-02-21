27542d69db9420c4c60dd8aa193123377aca19b8
Ronak Malik <ronakmalik4242@gmail.com>
2023-02-20 19:37:53 -0500

    Add client server headers

--- include/client.hpp
+++ include/client.hpp
@@ -1,4 +1,22 @@
+#pragma once
+
+#include "network.hpp"
+
 class Client
 {
-        
+public:
+
+    Client(std::string host, int port);
+
+    Network::Message messageCallback(Network::Message message);
+
+    void createAccount(std::string username);
+
+    void getAccountList();
+
+private:
+
+    int clientFd;
+
+    Network network;
 };
--- include/server.hpp
+++ include/server.hpp
@@ -1,4 +1,52 @@
+#pragma once
+
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "network.hpp"
+
+#define PORT 8080
+
 class Server
 {
+public:
+    Server(int port);
 
+    ///////////////////// Server functions /////////////////////
+
+    int acceptClient();
+
+    void stopServer();
+
+    //////////////////// Business functions ////////////////////
+
+    Network::Message createAccount(Network::Message info);
+
+    Network::Message listAccounts(Network::Message requester);
+
+    // Network::Message deleteAccount(Network::Message requester);
+
+    // Network::Message sendMessage(Network::Message message);
+    
+private:
+    int serverFd;
+
+    std::atomic<bool> serverRunning;
+
+    std::unordered_set<std::string> userList;
+
+    std::unordered_map<std::string, int> activeSockets;
+
+    std::unordered_map<int, std::mutex> socketLocks;
+
+    std::unordered_map<int, std::thread> socketThreads;
+
+    Network network;
+
+    int processClient(int socket);
 };
