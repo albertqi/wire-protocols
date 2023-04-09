93903df649942d16cd67b9ee69399751289aa8f0
Ronak Malik <ronakmalik4242@gmail.com>
2023-04-09 00:31:42 -0400

    Stash

--- src/serverMain.cpp
+++ src/serverMain.cpp
@@ -1,11 +1,46 @@
-#include "server.hpp"
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+
+#include "server.hpp"
 
 /**
  * Starts the server and accepts clients.
 */
 int main(int argc, char const *argv[])
 {
+    std::string server_config_path;
+    int replica_number;
+
+    if (argc < 3)
+    {
+        std::cerr << "Usage: ./server <server.cfg> <server_num>" << std::endl;
+        exit(1);
+    }
+
+     server_config_path = argv[1];
+
+    if (!std::filesystem::exists(server_config_path))
+    {
+        std::cerr << "Server config file doesn't exist." << std::endl;
+        exit(1);
+    }
+
+    try
+    {
+        replica_number = std::stoi(argv[2]);
+    }
+    catch (...)
+    {
+        std::cerr << "Server number must be a number." << std::endl;
+        exit(1);
+    }
+
+
+    std::ifstream server_cfg(server_config_path);
+
+    for (int i = 0; )
+
     Server server(1111);
 
     while (true)
